82ae314feb58f3188b6f8feda5cc076790379022
ElrohirGT <elrohirgt@gmail.com>
2024-12-03 16:33:10 -0600

    feat: Added basepath config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
 import "./style.css";
 import { Elm } from "./Main.elm";
 
-const basePath = import.meta.env.DEV ? null : "JUWURA";
+// NOTE: We need to use substring to remove the / at the beginning
+const basePath = import.meta.env.BASE_URL.substring(1);
 
 Elm.Main.init({
 	flags: basePath,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
 
 export default defineConfig({
+	base: "/JUWURA",
 	plugins: [
 		elmPlugin({
 			debug: false,
