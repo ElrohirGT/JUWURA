5af37d1795de4882c34cef66befceab2f742b413
ElrohirGT <elrohirgt@gmail.com>
2024-12-03 13:54:17 -0600

    feat: Added dummy test for ci

---
+++ backend/src/dummy.test.js
@@ -0,0 +1,9 @@
+import { describe, test, expect } from "vitest";
+
+describe("Dummy tests", () => {
+	test("1+1 = ?", () => {
+		const payload = 1;
+		const result = 1 + payload;
+		expect(result).toBe(2);
+	})
+});
