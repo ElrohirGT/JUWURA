d42c518eb446a6f591777311f6a78b6cb4ea6236
ElrohirGT <elrohirgt@gmail.com>
2024-12-03 12:00:59 -0600

    feat: Added basic tables

--- database/02_tables.sql
+++ database/02_tables.sql
@@ -8,11 +8,17 @@ COMMENT ON TABLE app_user IS 'Stores all the application users of JUWURA.';
 CREATE TABLE project (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(64) NOT NULL,
-	is_pinned BOOLEAN NOT NULL DEFAULT FALSE,
-	last_visited TIMESTAMP NOT NULL DEFAULT NOW(),
 	photo_url TEXT
 );
 COMMENT ON TABLE project IS 'Stores all projects in JUWURA';
+
+CREATE TABLE project_member (
+	project_id INTEGER REFERENCES project(id) NOT NULL,
+	user_id VARCHAR(100) REFERENCES app_user(email) NOT NULL,
+	is_pinned BOOLEAN NOT NULL DEFAULT FALSE,
+	last_visited TIMESTAMP NOT NULL DEFAULT NOW()
+);
+COMMENT ON TABLE project_member IS 'Stores all the members of a specific project';
 
 CREATE TABLE task_type (
 	name VARCHAR(16) PRIMARY KEY
@@ -29,9 +35,16 @@ CREATE TABLE task (
 	due_date TIMESTAMP,
 	status VARCHAR(16),
 	sprint INTEGER,
-	priority VARCHAR(16)
+	priority VARCHAR(16),
+	unblocks INTEGER REFERENCES task(id)
 );
 COMMENT ON TABLE task IS 'Stores all the task of all the projects, only some fields are required';
+
+CREATE TABLE task_asignee (
+	task_id INTEGER REFERENCES task(id) NOT NULL,
+	user_id VARCHAR(100) REFERENCES app_user(email) NOT NULL
+);
+COMMENT ON TABLE task_asignee IS 'Stores all the asigness for a given task';
 
 CREATE TABLE task_field_type (
 	name VARCHAR(16) PRIMARY KEY,
@@ -43,13 +56,14 @@ CREATE TABLE task_field (
 	id SERIAL PRIMARY KEY,
 	project_id INTEGER REFERENCES project(id) NOT NULL,
 	task_type VARCHAR(16) REFERENCES task_type(name) NOT NULL,
-	task_field_type VARCHAR(16) REFERENCES task_field_type(name) NOT NULL
+	task_field_type VARCHAR(16) REFERENCES task_field_type(name) NOT NULL,
+	name VARCHAR(16) NOT NULL
 );
 COMMENT ON TABLE task_field IS 'Stores all the custom fields of a given task type in a given project';
 
 CREATE TABLE task_field_option (
 	id SERIAL PRIMARY KEY,
-	task_field INTEGER REFERENCES task_field(id),
+	task_field INTEGER REFERENCES task_field(id) NOT NULL,
 	value TEXT
 );
 COMMENT ON TABLE task_field_option IS 'If a task field needs to select from multiple predefined values, the options to that task_field are saved here';
