17b098ea4ed0a7755c270a0bf2ae8fcfb6149b6a
ElrohirGT <elrohirgt@gmail.com>
2024-12-02 20:33:01 -0600

    fix: base path logic

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,8 @@
 import './style.css'
 import { Elm } from './Main.elm'
 
-const loc = window.location.pathname;
-const bPath = loc === "/" ? null : loc.substring(1, 8);
-console.log(`The supplied base path is: ${bPath}`)
-// Elm.Main.init({
-// 	flags: bPath
-// });
+const basePath = import.meta.env.DEV ? null : "JUWURA";
 
 Elm.Main.init({
-	flags: bPath
+	flags: basePath
 })
