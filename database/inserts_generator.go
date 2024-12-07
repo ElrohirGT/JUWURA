7f95b25ba34cc3179f4d87b2854bf26d7e5659dc
ElrohirGT <elrohirgt@gmail.com>
2024-12-07 12:29:33 -0600

    feat: renaming and added other inserts

--- database/inserts_generator.go
+++ database/inserts_generator.go
@@ -14,13 +14,13 @@ func main() {
 	seedText, err := reader.ReadString('\n')
 	var seed int64
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't read seed from stdin! Generating new one...")
+		fmt.Fprintf(os.Stderr, "Can't read seed from stdin! Generating new one...\n")
 		seed = rand.Int63()
 	} else {
 		seed, err = strconv.ParseInt(seedText, 10, 64)
 
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Can't parse seed from stdin! Generating new one...")
+			fmt.Fprintf(os.Stderr, "Can't parse seed from stdin! Generating new one...\n")
 		} else {
 			seed = rand.Int63()
 		}
@@ -31,11 +31,11 @@ func main() {
 
 	err = os.WriteFile(fmt.Sprintf("%d.seed", seed), []byte(fmt.Sprintf("%d\n", seed)), 0644)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Can't write the file saving the last seed!")
+		fmt.Fprintf(os.Stderr, "Can't write the file saving the last seed!\n")
 		panic(err)
 	}
 
-	fmt.Fprintf(os.Stderr, "Creating with seed: %d", seed)
+	fmt.Fprintf(os.Stderr, "Creating with seed: %d\n", seed)
 
 	booleans := []string{"true", "false"}
 	profilePictures := []string{
@@ -98,7 +98,8 @@ func main() {
 	}
 
 	// Generate app users...
-	userCount := 6
+	userCount := 8
+	generatedEmails := make([]string, userCount)
 	fmt.Println("INSERT INTO app_user (email, name, photo_url) VALUES")
 	for i := range userCount {
 		email := fmt.Sprintf("correo%d@gmail.com", i+1)
@@ -106,8 +107,10 @@ func main() {
 		photo := nullEveryPercent(random, 0.5, from(random, profilePictures))
 		fmt.Printf("('%s', '%s', %s)", email, name, photo)
 
+		generatedEmails[i] = email
 		endInserts(i, userCount)
 	}
+	// fmt.Fprintln(os.Stderr, generatedEmails)
 	fmt.Println()
 
 	// Generate projects...
@@ -123,7 +126,8 @@ func main() {
 	fmt.Println()
 
 	// Generate project members...
-	projectMembers := 2
+	projectMembers := 3
+	membersByProject := make([][]string, projectCount)
 	fmt.Println("INSERT INTO project_member (project_id, user_id, is_pinned, last_visited) VALUES")
 	for pIdx := range projectCount {
 		userIdxs := random.Perm(userCount)
@@ -132,6 +136,8 @@ func main() {
 			userId := userIdxs[i] + 1
 			isPinned := nullEveryPercent(random, 0.5, from(random, booleans))
 			fmt.Printf("(%d, %d, %s, NOW())", projectId, userId, isPinned)
+
+			membersByProject[pIdx] = append(membersByProject[pIdx], generatedEmails[userIdxs[i]])
 
 			endInserts((pIdx+1)*(i+1)-1, projectCount*projectMembers)
 		}
@@ -159,7 +165,42 @@ func main() {
 			endInserts((projectIdx+1)*(i+1)-1, projectCount*taskCount)
 		}
 	}
+	fmt.Println()
 
+	// Generate tasks unblocks...
+	fmt.Println("INSERT INTO task_unblock (target_task, unblocked_task) VALUES")
+	relationCount := 5
+	for projectIdx := range projectCount {
+		// Each project has #`taskCount` tasks,
+		// so we need to delta de IDs to match up to tasks on the same project.
+		delta := projectIdx * taskCount
+		for relIdx := range relationCount {
+			blockingTask := random.Intn(taskCount) + 1 + delta
+			unblockedTask := random.Intn(taskCount) + 1 + delta
+			fmt.Printf("(%d, %d)", blockingTask, unblockedTask)
+
+			endInserts((projectIdx+1)*(relIdx+1)-1, projectCount*relationCount)
+		}
+	}
+	fmt.Println()
+
+	// Generate task assignees...
+	fmt.Println("INSERT INTO task_asignee (task_id, user_id) VALUES")
+	assigneeCount := taskCount * 3 / 2
+	for projectIdx, members := range membersByProject {
+		tasksIdxs := random.Perm(taskCount)
+		membersIdxs := random.Perm(len(members))
+
+		for assIdx := range assigneeCount {
+			userId := members[membersIdxs[assIdx%len(membersIdxs)]]
+			taskId := tasksIdxs[assIdx%len(tasksIdxs)] + projectIdx*taskCount
+
+			fmt.Printf("(%d, '%s')", taskId, userId)
+
+			endInserts((projectIdx+1)*(assIdx+1)-1, assigneeCount*len(membersByProject))
+		}
+
+	}
 }
 
 func endInserts(i int, count int) {
