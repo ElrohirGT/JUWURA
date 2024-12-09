f9f707cfc0393f57ad8e69c66d7c3f8462730e88
ElrohirGT <elrohirgt@gmail.com>
2024-12-08 18:53:07 -0600

    fix: linting rules

--- database/02_tables.sql
+++ database/02_tables.sql
@@ -18,12 +18,14 @@ CREATE TABLE project_member (
     is_pinned BOOLEAN DEFAULT FALSE,
     last_visited TIMESTAMP NOT NULL
 );
-COMMENT ON TABLE project_member IS 'Stores all the members of a specific project';
+COMMENT ON TABLE project_member IS
+'Stores all the members of a specific project';
 
 CREATE TABLE task_type (
     name VARCHAR(16) PRIMARY KEY
 );
-COMMENT ON TABLE task_type IS 'Stores all the types a task can be, only contains EPIC, TASK, SUBTASK';
+COMMENT ON TABLE task_type IS
+'Stores all the types a task can be, only contains EPIC, TASK, SUBTASK';
 
 CREATE TABLE task (
     id SERIAL PRIMARY KEY,
@@ -37,26 +39,31 @@ CREATE TABLE task (
     sprint INTEGER,
     priority VARCHAR(16)
 );
-COMMENT ON TABLE task IS 'Stores all the task of all the projects, only some fields are required';
+COMMENT ON TABLE task IS
+'Stores all the task of all the projects, only some fields are required';
 
 CREATE TABLE task_unblock (
     target_task INTEGER REFERENCES task (id) NOT NULL,
     unblocked_task INTEGER REFERENCES task (id) NOT NULL
 );
-COMMENT ON TABLE task_unblock IS 'Stores all the tasks that unblock once target_task is completed';
+COMMENT ON TABLE task_unblock IS
+'Stores all the tasks that unblock once target_task is completed';
 
 CREATE TABLE task_assignee (
     task_id INTEGER REFERENCES task (id) NOT NULL,
     user_id VARCHAR(100) REFERENCES app_user (email) NOT NULL
 );
-COMMENT ON TABLE task_assignee IS 'Stores all the asigness for a given task';
+COMMENT ON TABLE task_assignee IS
+'Stores all the asigness for a given task';
 
 CREATE TABLE task_field_type (
     id SERIAL PRIMARY KEY,
     project_id INTEGER REFERENCES project (id),
     name VARCHAR(16) NOT NULL
 );
-COMMENT ON TABLE task_field_type IS 'Stores all the types of fields a task can have, for example DATE, TEXT, SELECT, etc';
+COMMENT ON TABLE task_field_type IS
+'Stores all the types of fields a task can have,
+for example DATE, TEXT, SELECT, etc';
 
 CREATE TABLE task_field (
     id SERIAL PRIMARY KEY,
@@ -65,18 +72,22 @@ CREATE TABLE task_field (
     task_field_type INTEGER REFERENCES task_field_type (id) NOT NULL,
     name VARCHAR(16) NOT NULL
 );
-COMMENT ON TABLE task_field IS 'Stores all the custom fields of a given task type in a given project';
+COMMENT ON TABLE task_field IS
+'Stores all the custom fields of a given task type in a given project';
 
 CREATE TABLE task_field_option (
     id SERIAL PRIMARY KEY,
     task_field INTEGER REFERENCES task_field (id) NOT NULL,
     value TEXT
 );
-COMMENT ON TABLE task_field_option IS 'If a task field needs to select from multiple predefined values, the options to that task_field are saved here';
+COMMENT ON TABLE task_field_option IS
+'If a task field needs to select from multiple predefined values,
+the options to that task_field are saved here';
 
 CREATE TABLE task_fields_for_task (
     task_id INTEGER REFERENCES task (id) NOT NULL,
     task_field_id INTEGER REFERENCES task_field (id) NOT NULL,
     value TEXT
 );
-COMMENT ON TABLE task_fields_for_task IS 'Relates all the custom task fields to a task';
+COMMENT ON TABLE task_fields_for_task IS
+'Relates all the custom task fields to a task';
