5d8a0bcd0b8a58979bd1832010199515007added
ElrohirGT <elrohirgt@gmail.com>
2024-12-11 19:22:55 -0600

    feat: Implemented backend to support db changes

--- backend/src/endpoints/project.integration.test.js
+++ backend/src/endpoints/project.integration.test.js
@@ -1,11 +1,16 @@
-import { describe, test, expect } from "vitest";
+import { describe, test } from "vitest";
 import { malformedJSON, noPayloadSupplied } from "./testHelpers";
 import { API_URL, createProject } from "./testHelpers/project";
 
 describe("Project POST integration tests", () => {
-	test("Successfully creates a project", async () => {
-		await createProject("JUWURA test project", "correo1@gmail.com");
-	});
+	test("Successfully creates a project", async () =>
+		await createProject(
+			"correo1@gmail.com",
+			"JUWURA test project",
+			"💀",
+			"https://cdn.lazyshop.com/files/d2c4f2c8-ada5-455a-86be-728796b838ee/other/192115ca73ec8c98c62e3cbc95b96d32.jpg",
+			[],
+		));
 	test("No payload supplied", noPayloadSupplied(API_URL));
 	test("Malformed JSON", malformedJSON(API_URL));
 });
--- backend/src/endpoints/testHelpers/project.js
+++ backend/src/endpoints/testHelpers/project.js
@@ -6,14 +6,20 @@ export const API_URL = `${API_HOST}/projects`;
 
 /**
  * Creates a project with the given name.
- * @param {string} name - The name of the project to create.
  * @param {string} userEmail - The email of the owner of the project.
- * @returns {number} The project ID
+ * @param {string} name - The name of the project to create.
+ * @param {string} icon - The icon of the project.
+ * @param {string[]} members - The emails of the members.
+ * @param {string} url - The banner URL.
+ * @returns {Promise<number>} The project ID
  */
-export async function createProject(name, userEmail) {
+export async function createProject(userEmail, name, icon, url, members) {
 	const payload = {
 		email: userEmail,
+		icon,
 		name,
+		members,
+		photo_url: url,
 		now_timestamp: new Date().getTime(),
 	};
 
@@ -25,10 +31,22 @@ export async function createProject(name, userEmail) {
 		console.error(response.data);
 	}
 
+	const expectedStructure = {
+		project: {
+			id: expect.any(Number),
+			name: expect.any(String),
+			photo_url: expect.any(String),
+			icon: expect.any(String),
+		},
+	};
+
 	expect(response.status).toBe(200);
-	expect(response.data.project.photo_url).toBeNull();
-	expect(response.data.project.id).toEqual(expect.any(Number));
-	expect(response.data.project.name).toEqual(payload.name);
+	expect(response.data).toEqual(expectedStructure);
+
+	const { project } = response.data;
+	expect(project.name).toBe(payload.name);
+	expect(project.icon).toBe(payload.icon);
+	expect(project.photo_url).toBe(payload.photo_url);
 
 	return response.data.project.id;
 }
