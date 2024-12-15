57b6b0d84a341ec0a7a1c870cc2fa836a3a15ce0
ElrohirGT <elrohirgt@gmail.com>
2024-12-15 14:54:20 -0600

    feat: Added error checking test

--- backend/src/zigLib/ws/task.test.js
+++ backend/src/zigLib/ws/task.test.js
@@ -44,6 +44,56 @@ describe.sequential("Create Task test suite", () => {
 		expect(task.project_id).toEqual(payload.create_task.project_id);
 	});
 
+	test("Create task error is sent only to request client", async () => {
+		const erroneousPayload = {
+			create_task: {
+				task_type: "EPIC",
+				project_id: 0, // This ID doesn't exists
+			},
+		};
+
+		const client1 = await generateClient("correo1@gmail.com", 1);
+
+		const client2 = await generateClient("correo2@gmail.com", 2);
+
+		const promise = new Promise((res, rej) => {
+			const client2Msgs = [];
+			const client1Msgs = [];
+
+			setTimeout(() => {
+				res([client1Msgs, client2Msgs]);
+			}, 4500);
+
+			client1.configureHandlers(rej, (rev) => {
+				try {
+					const data = JSON.parse(rev.toString());
+					client1Msgs.push(data);
+				} catch {}
+			});
+
+			client2.configureHandlers(rej, (rev) => {
+				try {
+					const data = JSON.parse(rev.toString());
+					client2Msgs.push(data);
+				} catch {}
+			});
+
+			client1.send(JSON.stringify(erroneousPayload));
+			client1.close();
+			client2.close();
+		});
+
+		/**@type{[any[], any[]]}*/
+		const [c1Msgs, c2Msgs] = await promise;
+
+		// The connection message is lost because the headers are set too late!
+		expect(c1Msgs.length).toBe(1);
+		expect(c2Msgs.length).toBe(1);
+
+		expect(c1Msgs[0]).toStrictEqual({ err: "CreateTaskError" });
+		expect(c2Msgs[0]).toStrictEqual({ user_connected: "correo2@gmail.com" });
+	});
+
 	test("Create task response is sent to all connected clients", async () => {
 		const payload = {
 			create_task: {
