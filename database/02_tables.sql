e3356c7fb6c675e27454ceecba2a999bba71ad58
ElrohirGT <elrohirgt@gmail.com>
2024-12-19 21:17:44 -0600

    feat: Added UNIQUE constraint

--- database/02_tables.sql
+++ database/02_tables.sql
@@ -45,6 +45,8 @@ CREATE TABLE task_field_type (
     id SERIAL PRIMARY KEY,
     project_id INTEGER REFERENCES project (id),
     name VARCHAR(16) NOT NULL
+
+		UNIQUE (project_id, name)
 );
 COMMENT ON TABLE task_field_type IS
 'Stores all the types of fields a task can have,
