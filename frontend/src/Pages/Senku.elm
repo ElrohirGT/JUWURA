a493441468675da0f6b7aeaf8698f9d68ae527bf
ElrohirGT <elrohirgt@gmail.com>
2024-12-23 11:13:00 -0600

    feat: Added view topbar with styles

--- frontend/src/Pages/Senku.elm
+++ frontend/src/Pages/Senku.elm
@@ -1,10 +1,10 @@
 module Pages.Senku exposing (view)
 
-import Css exposing (absolute, auto, backgroundColor, borderRadius4, calc, color, column, displayFlex, fitContent, flexDirection, fontFamilies, fontSize, height, left, maxWidth, padding, paddingLeft, plus, position, row, textTransform, top, uppercase, vh, vw, width, zero)
+import Css exposing (absolute, alignItems, backgroundColor, border, borderBottom3, borderColor, borderRadius, borderRadius4, borderWidth, color, displayFlex, fitContent, flexDirection, fontFamilies, fontSize, height, justifyContent, left, maxWidth, padding2, paddingBottom, paddingLeft, paddingRight, paddingTop, pct, position, px, row, solid, spaceBetween, stretch, top, vh, vw, width, zero)
 import CustomComponents.Icon.Icon as Icon
-import Html.Styled exposing (div, text)
+import Html.Styled exposing (button, div, text)
 import Html.Styled.Attributes exposing (css)
-import Theme exposing (cssColors, cssFontSizes, cssSpacing, fontSizes, spacing)
+import Theme exposing (cssColors, cssFontSizes, cssSpacing, spacing)
 import Utils exposing (viteAsset)
 
 
@@ -26,6 +26,7 @@ body =
         topbarHeightPct =
             64.0 / 1117.0 * 100.0
 
+        viewNavbarContainerStyles : List Css.Style
         viewNavbarContainerStyles =
             [ displayFlex
             , flexDirection row
@@ -33,7 +34,41 @@ body =
             , fontFamilies [ Theme.fontFamilies.label ]
             , fontSize cssFontSizes.label_extraLarge
             , maxWidth fitContent
+            , paddingBottom cssSpacing.xs
             ]
+
+        activeViewbarContainerStyles : List Css.Style
+        activeViewbarContainerStyles =
+            viewNavbarContainerStyles
+                ++ [ color cssColors.white_400
+                   , height (pct 100)
+                   , borderBottom3 cssSpacing.xs_3 solid cssColors.white_400
+                   ]
+
+        juwuraButton : List Css.Style
+        juwuraButton =
+            [ padding2 cssSpacing.xs_2 cssSpacing.l
+            , fontFamilies [ Theme.fontFamilies.title ]
+            , fontSize cssFontSizes.title_medium
+            , borderRadius cssSpacing.xs_2
+            ]
+
+        primaryButton : List Css.Style
+        primaryButton =
+            juwuraButton
+                ++ [ backgroundColor cssColors.red_600
+                   , color cssColors.white_50
+                   , border zero
+                   ]
+
+        secondaryButton : List Css.Style
+        secondaryButton =
+            juwuraButton
+                ++ [ backgroundColor cssColors.red_900
+                   , borderColor cssColors.red_300
+                   , borderWidth (px 1)
+                   , color cssColors.white_50
+                   ]
     in
     [ div
         [ css
@@ -55,19 +90,52 @@ body =
         , div
             [ css
                 [ paddingLeft (vw (sidebardWidthPct + 2))
-                , color cssColors.white_50
+                , paddingTop cssSpacing.xl
+                , borderBottom3 (px 1) solid cssColors.black_300
                 , displayFlex
                 , flexDirection row
-                , Css.property "gap" spacing.xl
+                , alignItems stretch
+                , justifyContent spaceBetween
+                , color cssColors.black_300
                 ]
             ]
-            [ div [ css viewNavbarContainerStyles ]
-                [ Icon.view (viteAsset "~icons/fa6-solid/table")
-                , text "BACKLOG"
+            [ div
+                [ css
+                    [ displayFlex
+                    , Css.property "gap" spacing.xl
+                    , alignItems stretch
+                    ]
                 ]
-            , div [ css viewNavbarContainerStyles ]
-                [ Icon.view (viteAsset "~icons/fa6-solid/share-nodes")
-                , text "OVERVIEW"
+                [ div [ css viewNavbarContainerStyles ]
+                    [ Icon.view (viteAsset "~icons/fa6-solid/table")
+                    , text "BACKLOG"
+                    ]
+                , div [ css activeViewbarContainerStyles ]
+                    [ Icon.view (viteAsset "~icons/fa6-solid/share-nodes")
+                    , text "OVERVIEW"
+                    ]
+                , div [ css viewNavbarContainerStyles ]
+                    [ Icon.view (viteAsset "~icons/fa6-solid/plus")
+                    ]
+                ]
+
+            -- Buttons
+            , div
+                [ css
+                    [ paddingBottom cssSpacing.xs
+                    , displayFlex
+                    , flexDirection row
+                    , Css.property "gap" spacing.l
+                    , alignItems stretch
+                    , paddingRight cssSpacing.xl_8
+                    ]
+                ]
+                [ button [ css secondaryButton ]
+                    [ text "SAVE VIEW"
+                    ]
+                , button [ css primaryButton ]
+                    [ text "NEW"
+                    ]
                 ]
             ]
         , -- Sidebar
