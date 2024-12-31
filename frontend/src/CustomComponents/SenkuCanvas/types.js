31b7d4000f12f5b06418e4a8dd75512b35063b9c
ElrohirGT <elrohirgt@gmail.com>
2024-12-31 13:12:24 -0600

    refactor: divided the code into more files

---
+++ frontend/src/CustomComponents/SenkuCanvas/types.js
@@ -0,0 +1,37 @@
+/**
+ * @typedef {Object} TaskStatus
+ * @property {string} name
+ * @property {string} color
+ */
+
+/**
+ * @typedef {Object} TaskData
+ * @property {number} id
+ * @property {Date} due_date
+ * @property {String} title
+ * @property {TaskStatus} status
+ * @property {String} icon
+ * @property {number} progress
+ */
+
+/**
+ * @typedef {Object} CellCoord
+ * @property {number} column
+ * @property {number} row
+ */
+
+/**
+ * @typedef {Object} TaskConnection
+ * @property {CellCoord} start
+ * @property {CellCoord} end
+ */
+
+/**
+ * @typedef {(TaskData|undefined)[][]} Cells
+ */
+
+/**
+ * @typedef {Object} SenkuCanvasState
+ * @property {Cells} cells
+ * @property {TaskConnection[]} connections
+ */
