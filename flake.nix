6a7b346f418991660a5eceddeca4c86f63766d12
ElrohirGT <elrohirgt@gmail.com>
2025-06-20 12:59:53 -0600

    feat: Added libs and tools

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
     # System packages...
     # backendPkgs = pkgs: [pkgs.zig pkgs.nodejs pkgs.pnpm pkgs.websocat];
-    backendPkgs = pkgs: [pkgs.gleam pkgs.erlang];
+    backendPkgs = pkgs: [pkgs.gleam pkgs.erlang pkgs.rebar3 pkgs.inotify-tools];
     dbPkgs = pkgs: [pkgs.sqlfluff];
     # frontendPkgs = pkgs: [pkgs.nodejs pkgs.pnpm pkgs.elmPackages.elm pkgs.elmPackages.elm-format pkgs.biome pkgs.elmPackages.elm-review];
     frontendPkgs = pkgs: [pkgs.gleam pkgs.biome];
